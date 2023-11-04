684ee1a7cee1352c80f48b3fefd1bb6614dfdc9b
Syed Tanzeem <syed.tanzeem@magnificenztech.com>
2023-11-04 19:08:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, NavLink , Link} from 'react-router-dom';
 import './App.css';
+import HMSQuote from './Pages/HMSQuote';
+import Home from './Pages/Home';
+import HMSInvoice from './Pages/HMSInvoice';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+<Router>
+  <Routes>
+  <Route exact path="/HMS" element={<HMSInvoice />} />
+  <Route exact path="/" element={<HMSInvoice />} />
+  </Routes>
+</Router>
     </div>
   );
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom"
+
+function Home() {
+
+
+    return (
+      <div className="">
+          <Link to='/HMSInvoice' className="button-fav">Invoice</Link>
+      </div>
+    );
+  }
+  
+  export default Home;
