3b6e5ee106917deddf0b933bedfb18d6c37ca6f2
Syed Tanzeem <syed.tanzeem@magnificenztech.com>
2023-11-18 13:09:21 +0530

    number alignment

--- src/Pages/HMSInvoice.js
+++ src/Pages/HMSInvoice.js
@@ -286,14 +286,14 @@ const HMSInvoice = () => {
     pdf.rect(134, 73, 20, 7);
     pdf.rect(154, 73, 43, 7);
     pdf.text(`01`, 17, 78);
-    pdf.text(`${formData.no1Item}`, 30, 78);
-    pdf.text(`${formData.no1Rate}`, 116, 78);
-    pdf.text(`${formData.qty1}`, 143, 78);
+    pdf.text(`${formData.no1Item}`, 30, 78,);
+    pdf.text(`${formData.no1Rate}`, 126, 78,'right');
+    pdf.text(`${formData.qty1}`, 146, 78,'right');
     let amount1 = formData.no1Rate * formData.qty1;
     if (isNaN(amount1) || amount1 === 0) {
       amount1 = "";
     }
-    pdf.text(`${amount1}`, 170, 78);
+    pdf.text(`${amount1}`, 178, 78,'right');
 
     //1
     pdf.rect(12, 66, 14, 7);
@@ -308,15 +308,15 @@ const HMSInvoice = () => {
     pdf.rect(134, 80, 20, 7);
     pdf.rect(154, 80, 43, 7);
     pdf.text(`02`, 17, 85);
-    pdf.text(`${formData.no2Item}`, 30, 85);
-    pdf.text(`${formData.no2Rate}`, 116, 85);
-    pdf.text(`${formData.qty2}`, 143, 85);
+    pdf.text(`${formData.no2Item}`, 30, 85,);
+    pdf.text(`${formData.no2Rate}`, 126, 85,'right');
+    pdf.text(`${formData.qty2}`, 146, 85,'right');
 
     let amount2 = formData.no2Rate * formData.qty2;
     if (isNaN(amount2) || amount2 === 0) {
       amount2 = "";
     }
-    pdf.text(`${amount2}`, 170, 85);
+    pdf.text(`${amount2}`, 178, 85,'right');
     //3
     pdf.rect(12, 87, 14, 7);
     pdf.rect(26, 87, 82, 7);
@@ -324,14 +324,14 @@ const HMSInvoice = () => {
     pdf.rect(134, 87, 20, 7);
     pdf.rect(154, 87, 43, 7);
     pdf.text(`03`, 17, 92);
-    pdf.text(`${formData.no3Item}`, 30, 92);
-    pdf.text(`${formData.no3Rate}`, 116, 92);
-    pdf.text(`${formData.qty3}`, 143, 92);
+    pdf.text(`${formData.no3Item}`, 30, 92,);
+    pdf.text(`${formData.no3Rate}`, 126, 92,'right');
+    pdf.text(`${formData.qty3}`, 146, 92,'right');
     let amount3 = formData.no3Rate * formData.qty3;
     if (isNaN(amount3) || amount3 === 0) {
       amount3 = "";
     }
-    pdf.text(`${amount3}`, 170, 92);
+    pdf.text(`${amount3}`, 178, 92,'right');
 
     //4
     pdf.rect(12, 87, 14, 7);
@@ -341,13 +341,13 @@ const HMSInvoice = () => {
     pdf.rect(154, 87, 43, 7);
     pdf.text(`04`, 17, 99);
     pdf.text(`${formData.no4Item}`, 30, 99);
-    pdf.text(`${formData.no4Rate}`, 116, 99);
-    pdf.text(`${formData.qty4}`, 143, 99);
+    pdf.text(`${formData.no4Rate}`, 126, 99,'right');
+    pdf.text(`${formData.qty4}`, 146, 99,'right');
     let amount4 = formData.no4Rate * formData.qty4;
     if (isNaN(amount4) || amount4 === 0) {
       amount4 = "";
     }
-    pdf.text(`${amount4}`, 170, 99);
+    pdf.text(`${amount4}`, 178, 99,'right');
     //5
     pdf.rect(12, 87, 14, 7);
     pdf.rect(26, 87, 82, 7);
@@ -356,15 +356,15 @@ const HMSInvoice = () => {
     pdf.rect(154, 87, 43, 7);
     pdf.text(`05`, 17, 106);
     pdf.text(`${formData.no5Item}`, 30, 106);
-    pdf.text(`${formData.no5Rate}`, 116, 106);
-    pdf.text(`${formData.qty5}`, 143, 106);
+    pdf.text(`${formData.no5Rate}`, 126, 106,'right');
+    pdf.text(`${formData.qty5}`, 146, 106,'right');
 
     let amount5 = formData.no5Rate * formData.qty5;
     if (isNaN(amount5) || amount5 === 0) {
       amount5 = "";
     }
 
-    pdf.text(`${amount5}`, 170, 106);
+    pdf.text(`${amount5}`, 178, 106,'right');
     //6
     pdf.rect(12, 94, 14, 7);
     pdf.rect(26, 94, 82, 7);
@@ -373,15 +373,15 @@ const HMSInvoice = () => {
     pdf.rect(154, 94, 43, 7);
     pdf.text(`06`, 17, 113);
     pdf.text(`${formData.no6Item}`, 30, 113);
-    pdf.text(`${formData.no6Rate}`, 116, 113);
-    pdf.text(`${formData.qty6}`, 143, 113);
+    pdf.text(`${formData.no6Rate}`, 126, 113,'right');
+    pdf.text(`${formData.qty6}`, 146, 113,'right');
 
     let amount6 = formData.no6Rate * formData.qty6;
     if (isNaN(amount6) || amount6 === 0) {
       amount6 = "";
     }
 
-    pdf.text(`${amount5}`, 170, 113);
+    pdf.text(`${amount6}`, 178, 113,'right');
     //7
     pdf.rect(12, 101, 14, 7);
     pdf.rect(26, 101, 82, 7);
@@ -390,14 +390,14 @@ const HMSInvoice = () => {
     pdf.rect(154, 101, 43, 7);
     pdf.text(`07`, 17, 120);
     pdf.text(`${formData.no7Item}`, 30, 120);
-    pdf.text(`${formData.no7Rate}`, 116, 120);
-    pdf.text(`${formData.qty7}`, 143, 120);
+    pdf.text(`${formData.no7Rate}`, 126, 120,'right');
+    pdf.text(`${formData.qty7}`, 146, 120,'right');
 
     let amount7 = formData.no7Rate * formData.qty7;
     if (isNaN(amount7) || amount7 === 0) {
       amount7 = "";
     }
-    pdf.text(`${amount7}`, 170, 120);
+    pdf.text(`${amount7}`, 178, 120,'right');
     //8
     pdf.rect(12, 108, 14, 7);
     pdf.rect(26, 108, 82, 7);
@@ -406,14 +406,14 @@ const HMSInvoice = () => {
     pdf.rect(154, 108, 43, 7);
     pdf.text(`08`, 17, 127);
     pdf.text(`${formData.no8Item}`, 30, 127);
-    pdf.text(`${formData.no8Rate}`, 116, 127);
-    pdf.text(`${formData.qty8}`, 143, 127);
+    pdf.text(`${formData.no8Rate}`, 126, 127,'right');
+    pdf.text(`${formData.qty8}`, 146, 127,'right');
 
     let amount8 = formData.no8Rate * formData.qty8;
     if (isNaN(amount8) || amount8 === 0) {
       amount8 = "";
     }
-    pdf.text(`${amount8}`, 170, 127);
+    pdf.text(`${amount8}`, 178, 127,'right');
     //9
     pdf.rect(12, 115, 14, 7);
     pdf.rect(26, 115, 82, 7);
@@ -422,14 +422,14 @@ const HMSInvoice = () => {
     pdf.rect(154, 115, 43, 7);
     pdf.text(`09`, 17, 134);
     pdf.text(`${formData.no9Item}`, 30, 134);
-    pdf.text(`${formData.no9Rate}`, 116, 134);
-    pdf.text(`${formData.qty9}`, 143, 134);
+    pdf.text(`${formData.no9Rate}`, 126, 134,'right');
+    pdf.text(`${formData.qty9}`, 146, 134,'right');
 
     let amount9 = formData.no9Rate * formData.qty9;
     if (isNaN(amount9) || amount9 === 0) {
       amount9 = "";
     }
-    pdf.text(`${amount9}`, 170, 134);
+    pdf.text(`${amount9}`, 178, 134,'right');
     //10
     pdf.rect(12, 122, 14, 7);
     pdf.rect(26, 122, 82, 7);
@@ -438,14 +438,14 @@ const HMSInvoice = () => {
     pdf.rect(154, 122, 43, 7);
     pdf.text(`10`, 17, 141);
     pdf.text(`${formData.no10Item}`, 30, 141);
-    pdf.text(`${formData.no10Rate}`, 116, 141);
-    pdf.text(`${formData.qty10}`, 143, 141);
+    pdf.text(`${formData.no10Rate}`, 126, 141,'right');
+    pdf.text(`${formData.qty10}`, 146, 141,'right');
 
     let amount10 = formData.no10Rate * formData.qty10;
     if (isNaN(amount10) || amount10 === 0) {
       amount10 = "";
     }
-    pdf.text(`${amount10}`, 170, 141);
+    pdf.text(`${amount10}`, 178, 141,'right');
     //11
     pdf.rect(12, 129, 14, 7);
     pdf.rect(26, 129, 82, 7);
@@ -454,14 +454,14 @@ const HMSInvoice = () => {
     pdf.rect(154, 129, 43, 7);
     pdf.text(`11`, 17, 148);
     pdf.text(`${formData.no11Item}`, 30, 148);
-    pdf.text(`${formData.no11Rate}`, 116, 148);
-    pdf.text(`${formData.qty11}`, 143, 148);
+    pdf.text(`${formData.no11Rate}`, 126, 148,'right');
+    pdf.text(`${formData.qty11}`, 146, 148,'right');
 
     let amount11 = formData.no11Rate * formData.qty11;
     if (isNaN(amount11) || amount11 === 0) {
       amount11 = "";
     }
-    pdf.text(`${amount11}`, 170, 148);
+    pdf.text(`${amount11}`, 178, 148,'right');
     //12
     pdf.rect(12, 136, 14, 7);
     pdf.rect(26, 136, 82, 7);
@@ -470,14 +470,14 @@ const HMSInvoice = () => {
     pdf.rect(154, 136, 43, 7);
     pdf.text(`12`, 17, 155);
     pdf.text(`${formData.no12Item}`, 30, 155);
-    pdf.text(`${formData.no12Rate}`, 116, 155);
-    pdf.text(`${formData.qty12}`, 143, 155);
+    pdf.text(`${formData.no12Rate}`, 126, 155,'right');
+    pdf.text(`${formData.qty12}`, 146, 155,'right');
 
     let amount12 = formData.no12Rate * formData.qty12;
     if (isNaN(amount12) || amount12 === 0) {
       amount12 = "";
     }
-    pdf.text(`${amount12}`, 170, 155);
+    pdf.text(`${amount12}`, 178, 155,'right');
     //13
     pdf.rect(12, 143, 14, 7);
     pdf.rect(26, 143, 82, 7);
@@ -486,14 +486,14 @@ const HMSInvoice = () => {
     pdf.rect(154, 143, 43, 7);
     pdf.text(`13`, 17, 162);
     pdf.text(`${formData.no13Item}`, 30, 162);
-    pdf.text(`${formData.no13Rate}`, 116, 162);
-    pdf.text(`${formData.qty13}`, 143, 162);
+    pdf.text(`${formData.no13Rate}`, 126, 162,'right');
+    pdf.text(`${formData.qty13}`, 146, 162,'right');
 
     let amount13 = formData.no13Rate * formData.qty13;
     if (isNaN(amount13) || amount13 === 0) {
       amount13 = "";
     }
-    pdf.text(`${amount13}`, 170, 162);
+    pdf.text(`${amount13}`, 178, 162,'right');
     //14
     pdf.rect(12, 150, 14, 7);
     pdf.rect(26, 150, 82, 7);
@@ -502,14 +502,14 @@ const HMSInvoice = () => {
     pdf.rect(154, 150, 43, 7);
     pdf.text(`14`, 17, 169);
     pdf.text(`${formData.no14Item}`, 30, 169);
-    pdf.text(`${formData.no14Rate}`, 116, 169);
-    pdf.text(`${formData.qty14}`, 143, 169);
+    pdf.text(`${formData.no14Rate}`, 126, 169,'right');
+    pdf.text(`${formData.qty14}`, 146, 169,'right');
 
-    let amount14 = formData.no14rRate * formData.qty14;
+    let amount14 = formData.no14Rate * formData.qty14;
     if (isNaN(amount14) || amount14 === 0) {
       amount14 = "";
     }
-    pdf.text(`${amount14}`, 170, 169);
+    pdf.text(`${amount14}`, 178, 169,'right');
     //15
     pdf.rect(12, 157, 14, 7);
     pdf.rect(26, 157, 82, 7);
@@ -518,14 +518,14 @@ const HMSInvoice = () => {
     pdf.rect(154, 157, 43, 7);
     pdf.text("15", 17, 176);
     pdf.text(`${formData.no15Item}`, 30, 176);
-    pdf.text(`${formData.no15Rate}`, 116, 176);
-    pdf.text(`${formData.qty15}`, 143, 176);
+    pdf.text(`${formData.no15Rate}`, 126, 176,'right');
+    pdf.text(`${formData.qty15}`, 146, 176,'right');
 
     let amount15 = formData.no15Rate * formData.qty15;
     if (isNaN(amount15) || amount15 === 0) {
       amount15 = "";
     }
-    pdf.text(`${amount15}`, 170, 176);
+    pdf.text(`${amount15}`, 178, 176,'right');
     //16
     pdf.rect(12, 164, 14, 7);
     pdf.rect(26, 164, 82, 7);
@@ -534,14 +534,14 @@ const HMSInvoice = () => {
     pdf.rect(154, 164, 43, 7);
     pdf.text(`16`, 17, 183);
     pdf.text(`${formData.no16Item}`, 30, 183);
-    pdf.text(`${formData.no16Rate}`, 116, 183);
-    pdf.text(`${formData.qty16}`, 143, 183);
+    pdf.text(`${formData.no16Rate}`, 126, 183,'right');
+    pdf.text(`${formData.qty16}`, 146, 183,'right');
 
     let amount16 = formData.no16Rate * formData.qty16;
     if (isNaN(amount16) || amount16 === 0) {
       amount16 = "";
     }
-    pdf.text(`${amount16}`, 170, 183);
+    pdf.text(`${amount16}`, 178, 183,'right');
     //17
     pdf.rect(12, 171, 14, 7);
     pdf.rect(26, 171, 82, 7);
@@ -550,14 +550,14 @@ const HMSInvoice = () => {
     pdf.rect(154, 171, 43, 7);
     pdf.text(`17`, 17, 190);
     pdf.text(`${formData.no17Item}`, 30, 190);
-    pdf.text(`${formData.no17Rate}`, 116, 190);
-    pdf.text(`${formData.qty17}`, 143, 190);
+    pdf.text(`${formData.no17Rate}`, 126, 190,'right');
+    pdf.text(`${formData.qty17}`, 146, 190,'right');
 
     let amount17 = formData.no17Rate * formData.qty17;
     if (isNaN(amount17) || amount17 === 0) {
       amount17 = "";
     }
-    pdf.text(`${amount17}`, 170, 190);
+    pdf.text(`${amount17}`, 178, 190,'right');
     //18
     pdf.rect(12, 178, 14, 7);
     pdf.rect(26, 178, 82, 7);
@@ -566,14 +566,14 @@ const HMSInvoice = () => {
     pdf.rect(154, 178, 43, 7);
     pdf.text(`18`, 17, 197);
     pdf.text(`${formData.no18Item}`, 30, 197);
-    pdf.text(`${formData.no18Rate}`, 116, 197);
-    pdf.text(`${formData.qty18}`, 143, 197);
+    pdf.text(`${formData.no18Rate}`, 126, 197,'right');
+    pdf.text(`${formData.qty18}`, 146, 197,'right');
 
     let amount18 = formData.no18Rate * formData.qty18;
     if (isNaN(amount18) || amount18 === 0) {
       amount18 = "";
     }
-    pdf.text(`${amount18}`, 170, 197);
+    pdf.text(`${amount18}`, 178, 197,'right');
     //19
     pdf.rect(12, 185, 14, 7);
     pdf.rect(26, 185, 82, 7);
@@ -582,14 +582,14 @@ const HMSInvoice = () => {
     pdf.rect(154, 185, 43, 7);
     pdf.text(`19`, 17, 204);
     pdf.text(`${formData.no19Item}`, 30, 204);
-    pdf.text(`${formData.no19Rate}`, 116, 204);
-    pdf.text(`${formData.qty19}`, 143, 204);
+    pdf.text(`${formData.no19Rate}`, 126, 204,'right');
+    pdf.text(`${formData.qty19}`, 146, 204,'right');
 
     let amount19 = formData.no19Rate * formData.qty19;
     if (isNaN(amount19) || amount19 === 0) {
       amount19 = "";
     }
-    pdf.text(`${amount19}`, 170, 204);
+    pdf.text(`${amount19}`, 178, 204,'right');
     //20
     pdf.rect(12, 192, 14, 7);
     pdf.rect(26, 192, 82, 7);
@@ -598,14 +598,14 @@ const HMSInvoice = () => {
     pdf.rect(154, 192, 43, 7);
     pdf.text(`20`, 17, 211);
     pdf.text(`${formData.no20Item}`, 30, 211);
-    pdf.text(`${formData.no20Rate}`, 116, 211);
-    pdf.text(`${formData.qty20}`, 143, 211);
+    pdf.text(`${formData.no20Rate}`, 126, 211,'right');
+    pdf.text(`${formData.qty20}`, 146, 211,'right');
 
     let amount20 = formData.no20Rate * formData.qty20;
     if (isNaN(amount20) || amount20 === 0) {
       amount20 = "";
     }
-    pdf.text(`${amount20}`, 170, 211);
+    pdf.text(`${amount20}`, 178, 211,'right');
 
     pdf.rect(12, 199, 14, 7);
     pdf.rect(26, 199, 82, 7);
@@ -646,7 +646,7 @@ const HMSInvoice = () => {
       amount18 +
       amount19 +
       amount20;
-    pdf.text(`${totalAmount}`, 165, 220);
+    pdf.text(`${totalAmount}`, 182, 220,"right");
 
     pdf.setFontSize(12);
 
@@ -668,8 +668,17 @@ const HMSInvoice = () => {
         pdf.addImage(Signature, "PNG", 140, 246, 50, 20);
         pdf.setTextColor(0,0,0);
         pdf.setFont(undefined, "none");
-        const dateFormat = new Date(formData.dateOfDelivery).toLocaleDateString('en-GB');
+        let dateFormat = new Date(formData.dateOfDelivery).toLocaleDateString('en-GB');
+        console.log(dateFormat)
+        if ( dateFormat === "Invalid Date") {
+          dateFormat = "";
+        }
+        pdf.setFontSize(12);
         pdf.text(`Date of Delivery: ${dateFormat} `, 14, 260);
+        if ( formData.deliveredBy === undefined) {
+          formData.deliveredBy = "";
+        }
+        pdf.text(`Delivered By: ${formData.deliveredBy} `, 14, 265);
 
     pdf.setFontSize(16);
    
@@ -741,12 +750,24 @@ const HMSInvoice = () => {
           <div className="form-group">
          <label>Date of Delivery</label> 
             <input
-              className="input "
+              className="input margin"
               type="date"
               id="dateOfDelivery"
               placeholder="dateOfDelivery"
               name="dateOfDelivery"
               value={formData.dateOfDelivery}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+         <label>Delivered By</label> 
+            <input
+              className="input "
+              type="text"
+              id="deliveredBy"
+              placeholder="Delivered By"
+              name="deliveredBy"
+              value={formData.deliveredBy}
               onChange={handleInputChange}
             />
           </div>
